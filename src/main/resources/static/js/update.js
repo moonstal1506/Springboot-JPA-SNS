9456d6ebe7b0c81ce6e0f25bf18aad6e7f848542
moonstal1506 <moonstal1506@gmail.com>
2022-03-17 22:00:26 +0900

    회원정보수정-유효성검사

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,5 +1,6 @@
 // (1) 회원정보 수정
-function update(userId) {
+function update(userId,event) {
+	event.preventDefault();
 	let data= $("#profileUpdate").serialize();
 	
 	console.log(data);
@@ -10,10 +11,10 @@ function update(userId) {
 		data:data,
 		contentType: "application/x-www-form-urlencoded; charset=utf-8",
 		dataType:"json"
-	}).done(res=>{
-		console.log("update 성공");
+	}).done(res=>{// HttpStatus 상태코드 200
+		console.log("update 성공",res);
 		location.href=`/user/${userId}`;
-	}).fail(error=>{
-		console.log("update 실패");
+	}).fail(error=>{//HttpStatus 상태코드 200 아닐때
+		alert(JSON.stringify(error.responseJSON.data));
 	});
 }
