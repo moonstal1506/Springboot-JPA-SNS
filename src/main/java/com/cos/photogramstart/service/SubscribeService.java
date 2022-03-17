f7fd57ece10ff3d7cb8fdb6101eb41c6aead6eaa
moonstal1506 <moonstal1506@gmail.com>
2022-03-18 02:13:48 +0900

    구독-예외처리

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.subscribe.Subscribe;
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
+import com.cos.photogramstart.handler.ex.CustomApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,7 +17,12 @@ public class SubscribeService {
 
 	@Transactional
 	public void 구독하기(int fromUserId, int toUserId) {
-		subscribeRepository.mSubscribe(fromUserId, toUserId);
+		try {
+			subscribeRepository.mSubscribe(fromUserId, toUserId);
+		} catch (Exception e) {
+			throw new CustomApiException("이미 구독하였습니다.");
+		}
+		
 	}
 	@Transactional
 	public void 구독취소하기(int fromUserId, int toUserId) {
