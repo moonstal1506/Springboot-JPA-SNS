1b6842d7fdc5bc62feea90508d80217ee60b979d
moonstal1506 <moonstal1506@gmail.com>
2022-03-17 15:49:29 +0900

    공통응답 Script

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
@@ -14,7 +15,12 @@ import com.cos.photogramstart.web.dto.CMRespDto;
 public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto<?> validationException(CustomValidationException e) {
-		return new CMRespDto<Map<String,String>>(-1,e.getMessage(),e.getErrorMap());
+	public String validationException(CustomValidationException e) {
+		
+		//CMRespDto, Script 비교
+		//1. 클라이언트에게 응답할 때는 Script좋음
+		//2. Ajax통신 - CMRespDto
+		//3. Android통신 - CMRespDto
+		return Script.back(e.getErrorMap().toString());
 	}
 }
