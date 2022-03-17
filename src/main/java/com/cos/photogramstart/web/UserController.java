3d16eed54492862909e119f4f024cdb281043bf0
moonstal1506 <moonstal1506@gmail.com>
2022-03-17 18:01:20 +0900

    로그인-세션정보 확인

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,8 +1,13 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
 
 @Controller
 public class UserController {
@@ -13,7 +18,14 @@ public class UserController {
 	}
 	
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id) {
+	public String update(@PathVariable int id,@AuthenticationPrincipal PrincipalDetails principalDetails) {
+		//1.추천
+		System.out.println("세션정보: "+principalDetails.getUser());
+		
+		//2.비추
+		Authentication auth=  SecurityContextHolder.getContext().getAuthentication();
+		PrincipalDetails mPrincipalDetails= (PrincipalDetails) auth.getPrincipal();
+		System.out.println("직접찾은 세션정보: "+mPrincipalDetails.getUser());
 		return "user/update";
 	}
 }
