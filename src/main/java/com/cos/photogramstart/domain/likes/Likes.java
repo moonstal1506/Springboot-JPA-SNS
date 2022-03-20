8f298015d891a478c84821095cc2c6605163cc26
moonstal1506 <moonstal1506@gmail.com>
2022-03-20 19:42:32 +0900

    likes모델

---
+++ src/main/java/com/cos/photogramstart/domain/likes/Likes.java
@@ -0,0 +1,50 @@
+package com.cos.photogramstart.domain.likes;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.subscribe.Subscribe;
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(uniqueConstraints = { @UniqueConstraint(name = "likes_uk", columnNames = { "imageId","userId" }) })
+public class Likes {//n
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	
+	@JoinColumn(name="imageId")
+	@ManyToOne
+	private Image image;//1
+	
+	@JoinColumn(name="userId")
+	@ManyToOne
+	private User user;
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist // db에 insert 되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+}
