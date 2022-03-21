2ffbea5fbf7e4314d38a39333828ef6a952b4c38
moonstal1506 <moonstal1506@gmail.com>
2022-03-22 02:55:15 +0900

    댓글-유효성 검사

--- src/main/java/com/cos/photogramstart/web/dto/comment/CommentDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/comment/CommentDto.java
@@ -1,11 +1,17 @@
 package com.cos.photogramstart.web.dto.comment;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class CommentDto {
+  //@NotEmpty  빈값이거나 null
+	@NotBlank //빈값이거나 null 빈공백 체크
 	private String content;
-	private int imageId;
+	@NotNull// null체크
+	private Integer imageId;
 	
 	//toEntity가 필요없다.
 }
