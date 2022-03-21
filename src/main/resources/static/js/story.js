d47bf25d1dd2699798e105fc031b3ac01ecd45fa
moonstal1506 <moonstal1506@gmail.com>
2022-03-21 17:14:53 +0900

    댓글-ajax

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -1,3 +1,4 @@
+
 /**
 	2. 스토리 페이지
 	(1) 스토리 로드하기
@@ -38,11 +39,9 @@ function getStoryItem(image) {
 		</div>
 		<div>${image.user.username}</div>
 	</div>
-
 	<div class="sl__item__img">
 		<img src="/upload/${image.postImageUrl}" />
 	</div>
-
 	<div class="sl__item__contents">
 		<div class="sl__item__contents__icon">
 			<button>`;
@@ -53,33 +52,24 @@ function getStoryItem(image) {
 				item+=`
 			</button>
 		</div>
-
 		<span class="like"><b id="storyLikeCount-${image.id}">${image.likeCount} </b>likes</span>
-
 		<div class="sl__item__contents__content">
 			<p>${image.caption}</p>
 		</div>
-
-		<div id="storyCommentList-1">
-
+		<div id="storyCommentList-${image.id}">
 			<div class="sl__item__contents__comment" id="storyCommentItem-1"">
 				<p>
 					<b>Lovely :</b> 부럽습니다.
 				</p>
-
 				<button>
 					<i class="fas fa-times"></i>
 				</button>
-
 			</div>
-
 		</div>
-
 		<div class="sl__item__input">
-			<input type="text" placeholder="댓글 달기..." id="storyCommentInput-1" />
-			<button type="button" onClick="addComment()">게시</button>
+			<input type="text" placeholder="댓글 달기..." id="storyCommentInput-${image.id}" />
+			<button type="button" onClick="addComment(${image.id})">게시</button>
 		</div>
-
 	</div>
 </div>`;
   return item;
@@ -147,20 +137,37 @@ function toggleLike(imageId) {
 }
 
 // (4) 댓글쓰기
-function addComment() {
-  let commentInput = $("#storyCommentInput-1");
-  let commentList = $("#storyCommentList-1");
+function addComment(imageId) {
+	let commentInput = $(`#storyCommentInput-${imageId}`);
+	let commentList = $(`#storyCommentList-${imageId}`);
 
-  let data = {
-    content: commentInput.val(),
-  };
+	let data = {
+		imageId:imageId,
+		content: commentInput.val(),
+	};
 
-  if (data.content === "") {
-    alert("댓글을 작성해주세요!");
-    return;
-  }
+	//console.log(data);
+	//console.log(JSON.stringify(data));
+	
 
-  let content = `
+	if (data.content === "") {
+		alert("댓글을 작성해주세요!");
+		return;
+	}
+
+	$.ajax({
+		type:"post",
+		url:"/api/comment",
+		data:JSON.stringify(data),
+		contentType:"application/json; charset=utf-8",
+		dataType:"json"
+	}).done(res => {
+			console.log("성공",res);
+	}).fail(error => {
+		console.log("오류", error);
+	});
+
+	let content = `
 			  <div class="sl__item__contents__comment" id="storyCommentItem-2""> 
 			    <p>
 			      <b>GilDong :</b>
@@ -169,9 +176,9 @@ function addComment() {
 			    <button><i class="fas fa-times"></i></button>
 			  </div>
 	`;
-  commentList.prepend(content);
-  commentInput.val("");
+	commentList.prepend(content);
+	commentInput.val("");
 }
 
 // (5) 댓글 삭제
-function deleteComment() {}
+function deleteComment() { }
